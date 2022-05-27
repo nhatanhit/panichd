7f417a7af37aa07643f120315c19462cfb17cbad
anh.tran5dev <anh.tran5@apac.merkleinc.com>
2022-05-27 09:26:32 +0700

    remove Agent Feature

--- src/Views/shared/nav.blade.php
+++ src/Views/shared/nav.blade.php
@@ -68,7 +68,6 @@
 			$tools->fullUrlIs(action('\PanicHD\PanicHD\Controllers\DashboardController@index').'*') ||
 			$tools->fullUrlIs(action('\PanicHD\PanicHD\Controllers\StatusesController@index').'*') ||
 			$tools->fullUrlIs(action('\PanicHD\PanicHD\Controllers\PrioritiesController@index').'*') ||
-			// $tools->fullUrlIs(action('\PanicHD\PanicHD\Controllers\AgentsController@index').'*') ||
 			$tools->fullUrlIs(action('\PanicHD\PanicHD\Controllers\MembersController@index').'*') ||
 			$tools->fullUrlIs(action('\PanicHD\PanicHD\Controllers\CategoriesController@index').'*') ||
 			$tools->fullUrlIs(action('\PanicHD\PanicHD\Controllers\NoticesController@index').'*') ||
